742d0e6613ac354b47cefed59dced54c327d48fc
Vipul-niteron <next.vipul2@gmail.com>
2025-04-01 17:39:42 +0530

    Clean up project: Remove test files, redundant scripts, and Docker-related files

--- test-api.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Opening WovenGold PDI API Tester in browser...
-start "" "test-form.html"
-echo.
-echo Remember: The API must be running for the test form to work!
-echo If the API is not running, start it with start-app.bat
-echo. 
